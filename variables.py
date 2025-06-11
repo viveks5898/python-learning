a94ea35b5d0a199e2ad93f1ee2fa2c6fbf39976e
viveks5898 <singh.vivek5898@gmail.com>
2025-06-11 20:32:41 +0530

    variables declaration

---
+++ variables.py
@@ -0,0 +1,10 @@
+
+name = "vivek singh"
+age =28
+is_student = False
+
+# print("name is" + age)  so this is the way to print the variables with  string
+
+# print(f"name is {name}")  so this is  the modern way to print  string with variable
+
+print("Name is {}".format(name))
