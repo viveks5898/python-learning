ded36dae000bf1434541d8c035091bde73edce62
viveks5898 <singh.vivek5898@gmail.com>
2025-06-11 20:39:03 +0530

    Capitalize the variables

--- vairables-exercise/exercise1.py
+++ vairables-exercise/exercise1.py
@@ -4,4 +4,4 @@ programming_language="python"
 
 # output should be looks like this= Hi, my name is Vivek. I am 25 years old and I love Python!
 
-print(f"Hi, my name is {name}. I am {age} years old and I love {programming_language}")
+print(f"Hi, my name is {name.capitalize()}. I am {age} years old and I love {programming_language.capitalize()}!")
