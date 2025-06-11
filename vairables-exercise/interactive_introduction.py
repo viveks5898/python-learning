41813ada781f2b77c9f2eae5daec10e82564f7ce
viveks5898 <singh.vivek5898@gmail.com>
2025-06-11 23:38:51 +0530

    added condtion rendering

--- vairables-exercise/interactive_introduction.py
+++ vairables-exercise/interactive_introduction.py
@@ -1,5 +1,28 @@
 name = input("Enter your name: ")
-age = input("Enter your age: ")
+
+try:
+    age = int(input("Enter your age: "))
+
+except ValueError:
+    print("Oops! Please enter a valid number for age.")
+    age=0
 language = input("Enter your favorite programming language: ")
 
 print(f"Hi, my name is {name.capitalize()}. I am {age} years old and I love {language.capitalize()}!")
+
+
+# second  example
+
+name = input("Enter your name: ")
+
+# Ask for age until a valid number is entered
+while True:
+    try:
+        age = int(input("Enter your age: "))
+        break  # ✅ correct input → exit the loop
+    except ValueError:
+        print("❌ Invalid input! Please enter a number for age.\n")
+
+language = input("Enter your favorite programming language: ")
+
+print(f"\nHi, my name is {name.capitalize()}. I am {age} years old and I love {language.capitalize()}!")
