647a3e8857f690c03e3d936ac12b80654ac6d659
viveks5898 <singh.vivek5898@gmail.com>
2025-06-11 20:45:51 +0530

    handling the input while print

---
+++ vairables-exercise/interactive_introduction.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+language = input("Enter your favorite programming language: ")
+
+print(f"Hi, my name is {name.capitalize()}. I am {age} years old and I love {language.capitalize()}!")
